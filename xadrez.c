1d48cff1adae47453caca75646394a521c731e36
Kethelly-beep <kethellybeatriz8@gmail.com>
2025-02-27 12:28:25 +0000

    verificando

--- xadrez.c
+++ xadrez.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 
-int main() {
-    int b, t, r, peca;
+void bispoM(int bispo){//função recursiva do bispo
+    if ( bispo > 0)
+    {
+        printf("Superior-Direita\n");
+        bispoM(bispo - 1);//chamando a função para realizar o cálculo pra impressão
+    }}
+void torreM(int torre){
+    if (torre > 0)
+    {
+        printf("Direita\n");
+        torreM(torre - 1);
+    }}
+
+void rainhaM(int rainha){
+    if (rainha > 0)
+    {
+        printf("Esquerda\n");
+        rainhaM(rainha - 1);
+    }}
+int main(){
+
+int c, peca;
 
 printf("***Escolha uma das peças abaixo***\n");
 printf("1- Bispo\n");
 printf("2- Torre\n");
 printf("3- Rainha\n");
+printf("4- Cavalo\n");
 printf("Digite sua escolha:\n");
 scanf("%d", &peca);
 
 switch (peca)
 {
 case 1:
-    printf("Peça: Bispo\n");
-for (b = 1; b <= 5; b++)
-{
-    printf("Superior-Direita\n");
-}
+printf("Peça: Bispo\n");
+bispoM(5);
     break;
 
 case 2:
-
 printf("Peça: Torre\n");
-
-for(t= 1; t <= 5; t++)
-{
-    printf("Direita\n");
-}
+torreM(5);
 break;
 
 case 3:
 printf("Peça: Rainha\n");
-for (r = 1; r<=8; r++)
-{
-    printf("Esquerda\n");
-}
+rainhaM(8);
 break;
 
 case 4:
@@ -49,9 +59,10 @@ while ( cc--)
     }
     printf("Esquerda\n");
 }
+break;
 
 default:
-printf("Essa opção não existe!\n");
+printf("Essa opção não existe!");
     break;
 }
     return 0;
